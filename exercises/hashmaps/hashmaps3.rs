d2e9c501347a5fab980a9ebf92391e2c26dbd741
puraw <monolith@LAPTOP-HQSDP4MR.localdomain>
2024-04-12 19:12:24 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,18 +39,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        score = scores.entry(team_1_name.clone()).or_insert(Team{
+        let score = scores.entry(team_1_name.clone()).or_insert(Team{
                 goals_scored: 0,
                 goals_conceded : 0,
         }); 
-        score.goals_conceded+=team_1_score
-        score.goals_conceded+=team_2_score
-        score = scores.entry(team_1_name.clone()).or_insert(Team{
+        (*score).goals_scored+=team_1_score;
+        (*score).goals_conceded+=team_2_score;
+        let score = scores.entry(team_2_name.clone()).or_insert(Team{
             goals_scored: 0,
             goals_conceded : 0,
         }); 
-        score.goals_conceded+=team_1_score
-        score.goals_conceded+=team_2_score    
+        (*score).goals_scored +=team_2_score;
+        (*score).goals_conceded+=team_1_score;   
     }
     scores
 }
@@ -83,9 +83,9 @@ mod tests {
     #[test]
     fn validate_team_score_1() {
         let scores = build_scores_table(get_results());
-        let team = scores.get("England").unwrap();
-        assert_eq!(team.goals_scored, 5);
-        assert_eq!(team.goals_conceded, 4);
+        let team = scores.get("England");
+        assert_eq!(team.unwrap().goals_scored, 5);
+        assert_eq!(team.unwrap().goals_conceded, 4);
     }
 
     #[test]
